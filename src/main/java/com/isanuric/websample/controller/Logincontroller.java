ec2c4ad1e4a1c3e9c3f94ccf6658959008cb794d
Ehsan Salmani <ehsan.salmani@ic-consult.de>
2018-12-25 21:44:41 +0100

    add delete user

--- src/main/java/com/isanuric/websample/controller/Logincontroller.java
+++ src/main/java/com/isanuric/websample/controller/Logincontroller.java
@@ -2,12 +2,19 @@ package com.isanuric.websample.controller;
 
 import com.isanuric.websample.user.User;
 import com.isanuric.websample.user.UserRepository;
-import java.util.Collection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Optional;
-import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/web-app")
 public class Logincontroller {
-    
+
     private UserRepository userRepository;
 
     public Logincontroller(UserRepository userRepository) {
@@ -40,10 +47,23 @@ public class Logincontroller {
     public Iterable<User> getAllUser() {
         return this.userRepository.findAll();
     }
-    
+
     @GetMapping("/user/{id}")
     public Optional<User> getUserById(@PathVariable Long id) {
         return this.userRepository.findById(id);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> addUser(@Valid @RequestBody String user) throws MalformedURLException, URISyntaxException {
+        User newUser = this.userRepository.save(new User(user));
+        return ResponseEntity.created(new URI("/web-app/user/" + newUser.getId()))
+                .body(newUser);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        this.userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 //    @GetMapping("/")
