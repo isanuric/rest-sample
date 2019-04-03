9632dc536e91d4e898d118c1fa55e55076785f3d
Ehsan Salmani <ehsan.salmani@ic-consult.de>
2019-04-03 18:40:29 +0200

    add webTestClient

--- src/test/java/com/isanuric/websample/WebSampleApplicationTests.java
+++ src/test/java/com/isanuric/websample/WebSampleApplicationTests.java
@@ -3,16 +3,34 @@ package com.isanuric.websample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class WebSampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    protected WebTestClient webTestClient;
+
+    @Test
+    public void getUserById() {
+
+        webTestClient
+                .get().uri("/users")
+                .exchange()
+
+                // validate
+                .expectStatus().isOk()
+                .expectBody(String.class).consumeWith(v -> {
+            System.out.println(v);
+//            assertTrue(v.getResponseBody().contains("AAAA"));
+        });
+    }
 
 }
 
